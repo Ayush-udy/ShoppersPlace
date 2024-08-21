297843c5af8eb5513163ccc2726e04b8626136ae
Ayush Upadhyay <114952863+Ayush-udy@users.noreply.github.com>
2024-08-21 15:42:10 +0530

    Updates

--- server.js
+++ server.js
@@ -30,8 +30,6 @@ app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product', productRoutes);
 
-
-
 //rest api
 app.get('/', (req, res)=>{
     res.send("<h1>Welcome to ShoppersPlace App!</h1>")
